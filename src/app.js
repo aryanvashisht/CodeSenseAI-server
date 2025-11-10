0533c4746546ca0355ed14f727563c6273e90508
aryanvashisht <vashishtaryan179@gmail.com>
2025-11-10 11:14:07 +0530

    fix: update CORS middleware to use app.options for preflight requests

--- src/app.js
+++ src/app.js
@@ -8,13 +8,14 @@ const app = express(); // creates server does not start it.
 
 app.use(express.json());
 
-app.use(cors({
+app.options("*", cors({
   origin: "http://localhost:5173",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
   maxAge: 86400
 }));
+
 
 
 app.use("/api/v1/ai",aiRoutes);
