a884da1f44f1fbe3959db93244eab77dd49b8185
aryanvashisht <vashishtaryan179@gmail.com>
2025-11-10 14:00:09 +0530

    fix: simplify CORS configuration by removing specific options for preflight requests

--- src/app.js
+++ src/app.js
@@ -8,18 +8,10 @@ const app = express(); // creates server does not start it.
 
 app.use(express.json());
 
-app.options("/*", cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-  maxAge: 86400
-}), (req, res) => {
-  // End the OPTIONS request with 204 No Content
-  res.sendStatus(204);
-});
-
-
+app.use(cors({
+  origin: "*",
+  credentials: true
+}))
 
 app.use("/api/v1/ai",aiRoutes);
 
