6c95e2a7963ede452323a02434410004bca81edb
aryanvashisht <vashishtaryan179@gmail.com>
2025-11-10 12:14:01 +0530

    fix: enable CORS preflight handling with custom headers

--- src/app.js
+++ src/app.js
@@ -8,13 +8,25 @@ const app = express(); // creates server does not start it.
 
 app.use(express.json());
 
-app.options("*", cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-  maxAge: 86400
-}));
+// app.options("*", cors({
+//   origin: "",
+//   methods: ["GET", "POST", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true,
+//   maxAge: 86400
+// }));
+
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 
 
 
