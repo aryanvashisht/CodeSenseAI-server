c6f043b21ef4285a92602a8bd433293ac93f75af
aryanvashisht <vashishtaryan179@gmail.com>
2025-11-10 18:51:44 +0530

    fix: streamline CORS middleware by removing custom origin handling and simplifying configuration

--- src/app.js
+++ src/app.js
@@ -8,40 +8,16 @@ const app = express(); // creates server does not start it.
 
 app.use(express.json());
 
-const allowedOrigins = [
-    "http://localhost:5173"
-    //   ""       // frontend domain
-];
+app.use(cors({
+  origin: ' http://localhost:5173/', 
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, 
+}));
 
-app.use(express.json());
+app.use("/api/v1/ai",aiRoutes);
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
-
-app.options("*", (req, res) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(200);
-});
-
-app.use("/api/v1/ai", aiRoutes);
-
-app.get("/fine", (req, res) => {
+app.get("/fine",(req,res)=>{
     res.send({
         pass: "test pass"
     })
