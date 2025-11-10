9c38c9d89e9264017b5b57860f98bc77721942f9
aryanvashisht <vashishtaryan179@gmail.com>
2025-11-10 14:32:22 +0530

    fix: update CORS configuration to restrict origins and improve security

--- src/app.js
+++ src/app.js
@@ -8,14 +8,29 @@ const app = express(); // creates server does not start it.
 
 app.use(express.json());
 
-app.use(cors({
-  origin: "*",
-  credentials: true
-}))
+const allowedOrigins = [
+    "http://localhost:5173"
+    //   ""       // frontend domain
+];
 
-app.use("/api/v1/ai",aiRoutes);
+app.use(express.json());
 
-app.get("/fine",(req,res)=>{
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+    })
+);
+
+app.use("/api/v1/ai", aiRoutes);
+
+app.get("/fine", (req, res) => {
     res.send({
         pass: "test pass"
     })
